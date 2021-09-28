edd6d19b744215246a81ace0c4752c2bee29c296
Andrés K. Aquin <andruuus123@gmail.com>
2021-09-28 13:57:04 -0400

    Mostrar a cada dueño con sus mascotas juntando Listas

--- LAB131C/src/ListaSimple1/Main.java
+++ LAB131C/src/ListaSimple1/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		LSimpleD A=new LSimpleD();
 		A.adifinal("ANA", 2);
 		A.adifinal("LUIS", 1);
-		A.adifinal("JUAN", 1);
+		A.adifinal("JUAN", 2);
 		A.adifinal("MARIA", 2);
 		System.out.println("\nLISTA DE DUEÑOS");
 		A.mostrar();
@@ -16,8 +16,9 @@ public class Main {
 		Mascota m2=new Mascota("niki","perro",8);
 		Mascota m3=new Mascota("pelusa","gato",5);
 		Mascota m4=new Mascota("tito","loro",1);
-		Mascota m5=new Mascota("kiti","loro",4);
-		Mascota m6=new Mascota("lulu","tortuga",10);
+		Mascota m5=new Mascota("tuto","loro",2);
+		Mascota m6=new Mascota("kiti","loro",4);
+		Mascota m7=new Mascota("lulu","tortuga",10);
 		System.out.println("\nLISTA DE MASCOTAS");
 		B.adifinal(m1);
 		B.adifinal(m2);
@@ -25,8 +26,26 @@ public class Main {
 		B.adifinal(m4);
 		B.adifinal(m5);
 		B.adifinal(m6);
+		B.adifinal(m7);
 		B.mostrar();
-		
+		//mostrar a cada dueño con sus mascotas
+		sol1(A,B);
+	}
+
+	private static void sol1(LSimpleD a, LSimpleM b) {
+		// TODO Auto-generated method stub
+		NodoD r1=a.getP();
+		NodoM r2=b.getP();
+		while(r1!=null)
+		{
+			System.out.println("\nDUEÑO:  ["+r1.getNom()+" "+r1.getNroMascotas()+"]");
+			for(int i=1; i<=r1.getNroMascotas();i++)
+			{
+				r2.getM().mostrar();
+				r2=r2.getSig();
+			}
+			r1=r1.getSig();
+		}
 	}
 
 }
